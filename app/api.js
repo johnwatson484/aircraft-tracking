839b549c1eff9c7e9e3c97fdc15f5025c220b7f0
John Watson <johnwatson484@hotmail.com>
2021-12-29 21:30:05 +0000

    Add log

--- app/api.js
+++ app/api.js
@@ -4,12 +4,13 @@ const { api } = require('./config')
 const get = async (path) => {
   const { protocol, host, username, password } = api
   const request = `${protocol}://${username}:${password}@${host}/${path}`
+  console.log('Tracking')
   console.log(request)
   const { payload } = await wreck.get(request, getConfiguration())
   return payload
 }
 
-const getConfiguration = (token = '') => {
+const getConfiguration = () => {
   return {
     json: true
   }
