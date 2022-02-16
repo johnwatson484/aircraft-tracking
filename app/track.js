605f260096b28e952ad8bc7d9c149e35662906ca
John Watson <johnwatson484@hotmail.com>
2022-02-16 21:21:57 +0000

    Log removed

--- app/track.js
+++ app/track.js
@@ -15,7 +15,6 @@ const start = async () => {
 }
 
 const trackAircraft = async () => {
-  console.log('Scanning')
   const bbox = getBoundingBox()
   const { lamin, lomin, lamax, lomax } = bbox
   const aircraft = await get(`states/all?lamin=${lamin}&lomin=${lomin}&lamax=${lamax}&lomax=${lomax}`)
